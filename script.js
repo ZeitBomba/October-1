dd92d3b760f610e0cabffbbeb00bef8a0a5ce01c
ZeitBomba <realtrolli2001@gmail.com>
2025-10-01 15:06:11 +0800

    Added the CSS and JS files without the folder.
    This should work now, right?

---
+++ script.js
@@ -0,0 +1,15 @@
+// const img = document.querySelector("img")
+
+// img.addEventListener("mouseenter", () => {
+//   document.body.style.backgroundColor = "pink";
+// //   img.style.transform = "scale(1.05)";
+// });
+
+// img.addEventListener("mouseleave", () => {
+//   document.body.style.backgroundColor = "black";
+// //   img.style.transform = "scale(1)";
+// });
+
+
+
+
